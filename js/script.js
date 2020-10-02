1286cd9590e8b9c7818ffc99b47d1b341580e64a
yoann stas <yoann.stas@me.com>
2020-10-02 11:33:08 +0200

    feat: Create index.html

---
+++ js/script.js
@@ -0,0 +1,41 @@
+function countUp(maxNum, id, timeInterval) {
+    let display = 0
+    let interval = setInterval(function counting() {
+        display += 1;
+        document.getElementById(id).innerHTML = display
+
+        if (display === maxNum
+        ) {
+            clearInterval(interval);
+        }
+    }, timeInterval);
+}
+countUp(35, 'age', 300);
+
+$(window).scroll(function() {
+    $('.fade-in').each(function() {
+        var top_of_element = $(this).offset().top;
+        var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+        var top_of_screen = $(window).scrollTop();
+
+        if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element) && !$(this).hasClass('is-visible')) {
+            $(this).addClass('is-visible');
+        }
+    });
+});
+
+var myDate = new Date();
+var hrs = myDate.getHours();
+
+var greet;
+
+if (hrs < 12)
+    greet = 'Good Morning!';
+else if (hrs >= 12 && hrs <= 17)
+    greet = 'Good Afternoon!';
+else if (hrs >= 17 && hrs <= 24)
+    greet = 'Good Evening!';
+
+document.getElementById('greeting').innerHTML =
+    '<b>' + greet + '</b> <br>I am Yoann. <br> A multidisciplinary designer.';
